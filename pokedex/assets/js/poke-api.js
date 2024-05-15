7754084aa28c01f56929d31b974794a8f5dfd87b
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-05-15 16:15:08 -0300

    adicionando os tipos de pokemon dinamicamente

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -4,9 +4,25 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.order;
+    pokemon.name = pokeDetail.name;
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    const [type] = types
+
+    pokemon.types = types;
+    pokemon.type = type;
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+    return pokemon;
+}
 pokeApi.getPokemonsDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset=0, limit=5) => {
@@ -19,12 +35,3 @@ pokeApi.getPokemons = (offset=0, limit=5) => {
         .then((pokemonDetails) => pokemonDetails)
         .catch((error) => console.log(error))
 }
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4')
-]).then(results => {
-    console.log(results);
-});
